4c72baf7e62cfb0a8554dbcd9e2497e5e9eb35ee
Thainá Pires <thainaspiress@gmail.com>
2025-03-24 01:26:36 -0300

    refac: return fonts to layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,37 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
+import { Inter, Poppins, Rubik } from "next/font/google";
 import { Footer } from "./components/layout/footer";
 import { Header } from "./components/layout/header";
-import { appFonts } from "./constants";
 import "./globals.css";
 import { queryClient } from "./lib/react-query";
 
-const fonts = appFonts;
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+})
+
+const rubik = Rubik({
+  variable: '--font-rubik',
+  subsets: ["latin"],
+})
+
+const poppins = Poppins({
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: '400'
+})
+
+export const metadata: Metadata = {
+  title: "Thainá Pires - Portfolio",
+  description: "Portfolio of Thainá Pires",
+};
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${fonts.inter.variable} ${fonts.rubik.variable} ${fonts.poppins.variable} antialiased`} suppressHydrationWarning>
+      <body className={`${inter.variable} ${rubik.variable} ${poppins.variable} antialiased`} suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}> 
           <QueryClientProvider client={queryClient}>
             <Header />
